a606fd38270ffe24650da2bb4925b2c0474c026d
Maike <maike_jbe@hotmail.com>
2018-06-13 17:52:58 -0300

    Alteração em implementação de métodos

--- app/Places.php
+++ app/Places.php
@@ -5,18 +5,18 @@ use Ciebit\Places\Repositories\IBGE\Repository as IBGERepository;
 
 class Places
 {
-    public const CITY_ID = null; #string
-    public const CITY_INITIALS = null; #string
-    public const CITY_NAME = null; #string
-    public const COUNTRY_ID = null; #string
-    public const COUNTRY_INITIALS = null; #string
-    public const COUNTRY_NAME = null; #string
-    public const STATE_ID = null; #string
-    public const STATE_INITIALS = null; #string
-    public const STATE_NAME = null; #string
+    public const CITY_ID = 'city-id';
+    public const CITY_NAME = 'city-name';
+    public const COUNTRY_ID = 'country-id';
+    public const COUNTRY_ABBREVIATION = 'country-abbreviation';
+    public const COUNTRY_NAME = 'country-name';
+    public const STATE_ID = 'state-id';
+    public const STATE_ABBREVIATION = 'state-abbreviation';
+    public const STATE_NAME = 'state-name';
     public const TOTAL = null; #string
 
     private $Repository; #Repositories
+    private $filters; #array
 
     public function __construct(Repositories $Repository = null)
     {
@@ -29,7 +29,7 @@ class Places
 
     public function addFilter(string $codeColumn, $value): self
     {
-        $codeColumn = $value;
+        $this->filters[$codeColumn] = $value;
         return $this;
     }
 
@@ -56,22 +56,43 @@ class Places
 
     private function applyCitiesFilters(Repository $Repository)
     {
-        $Repository->setFilterId(CITY_ID);
-        $Repository->setFilterName(CITY_NAME);
-        $Repository->setFilterInitial(CITY_INITIALS);
+        if ($this->filters[CITY_ID])
+            {$Repository->setFilterId($this->filters[CITY_ID]);}
+
+        if ($this->filters[CITY_NAME])
+            {$Repository->setFilterName($this->filters[CITY_NAME]);}
+
+        if ($this->filters[STATE_ID])
+            {$Repository->setFilterStateId($this->filters[STATE_ID]);}
+
+        if ($this->filters[STATE_NAME])
+            {$Repository->setFilterStateName($this->filters[STATE_NAME]);}
+
+        if ($this->filters[STATE_ABBREVIATION])
+            {$Repository->setFilterStateAbbreviation($this->filters[STATE_ABBREVIATION]);}
     }
 
     private function applyStatesFilters(Repository $Repository)
     {
-        $Repository->setFilterId(STATE_ID);
-        $Repository->setFilterName(STATE_NAME);
-        $Repository->setFilterInitial(STATE_INITIALS);
+        if ($this->filters[STATE_ID])
+            {$Repository->setFilterId($this->filters[STATE_ID]);}
+
+        if ($this->filters[STATE_NAME])
+            {$Repository->setFilterName($this->filters[STATE_NAME]);}
+
+        if ($this->filters[STATE_ABBREVIATION])
+            {$Repository->setFilterAbbreviation($this->filters[STATE_ABBREVIATION]);}
     }
 
     private function applyCountriesFilters(Repository $Repository)
     {
-        $Repository->setFilterId(COUNTRY_ID);
-        $Repository->setFilterName(COUNTRY_NAME);
-        $Repository->setFilterInitial(COUNTRY_INITIALS);
+        if ($this->filters[COUNTRY_ID])
+            {$Repository->setFilterId($this->filters[COUNTRY_ID]);}
+
+        if ($this->filters[COUNTRY_NAME])
+            {$Repository->setFilterName($this->filters[COUNTRY_NAME]);}
+
+        if ($this->filters[COUNTRY_ABBREVIATION])
+            {$Repository->setFilterAbbreviation($this->filters[COUNTRY_ABBREVIATION]);}
     }
 }
