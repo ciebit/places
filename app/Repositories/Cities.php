dc577313db047ea587b80eccc93dfba5b5a9f220
Maike <maike_jbe@hotmail.com>
2018-06-13 17:59:36 -0300

    Criação de Interface do Repositório de Cidades

---
+++ app/Repositories/Cities.php
@@ -0,0 +1,13 @@
+<?php
+namespace Ciebit\Places\Repositories;
+
+interface Cities
+{
+    public function get(): CitiesCollection;
+    public function setFilterId(int $id): self;
+    public function setFilterName(string $name, bool $identical = true): self;
+    public function setFilterStateId(int $id): self;
+    public function setFilterStateAbbreviation(string $abbreviation): self;
+    public function setFilterStateName(string $name): self;
+    public function setTotal(int $total): self;
+}
