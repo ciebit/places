1340514f93967b9296e98ac6fc920c6d69fce0b6
Maike <maike_jbe@hotmail.com>
2018-06-13 17:56:47 -0300

    Criação de Classe

---
+++ app/Collections/Cities.php
@@ -0,0 +1,33 @@
+<?php
+namespace Ciebit\Places\Collections;
+
+class Cities
+{
+    private $Cities; #ArrayObject
+
+    public function __construct()
+    {
+        $this->Cities = new ArrayObject;
+    }
+
+    public function add(City $city): self
+    {
+        $this->Cities->append($city);
+    }
+
+    public function getArrayIterator(): ArrayIterator
+    {
+        $this->Cities->getIterator;
+    }
+
+    public function getArrayObject(): ArrayObject
+    {
+        $clone = $this->Cities;
+        return $clone;
+    }
+
+    public function total(): int
+    {
+        return $this->Cities->count();
+    }
+}
