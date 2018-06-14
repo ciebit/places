e7e936c52324e16113d9b131633eb4d8fd9d5e0e
Maike <maike_jbe@hotmail.com>
2018-06-14 18:00:17 -0300

    Correção da lógica de filtragem e de retorno de métodos

--- app/Repositories/IBGE/Cities.php
+++ app/Repositories/IBGE/Cities.php
@@ -4,6 +4,8 @@ namespace Ciebit\Places\Repositories\IBGE;
 use Ciebit\Places\Repositories\Cities as ICities;
 use Ciebit\Places\Collections\Cities as CitiesCollection;
 use Ciebit\Places\City;
+use Ciebit\Places\State;
+use Ciebit\Places\Country;
 
 class Cities implements ICities
 {
@@ -20,134 +22,142 @@ class Cities implements ICities
     {
         $this->Cities = new CitiesCollection;
 
-        if (! $this->applyFilters()) {
-            $this->getAll();
-        }
-
-        return $this->Cities;
+        return $this->fetch();
     }
 
-    public function setFilterId(int $id): self
+    public function setFilterId(int $id): ICities
     {
         $this->filterId = $id;
         return $this;
     }
 
-    public function setFilterName(string $name, bool $identical = true): self
+    public function setFilterName(string $name, bool $identical = true): ICities
     {
         $this->filterName = $name;
         return $this;
     }
 
-    public function setFilterStateId(int $id): self
+    public function setFilterStateId(int $id): ICities
     {
-        $this->$filterStateId = $id;
+        $this->filterStateId = $id;
         return $this;
     }
 
-    public function setFilterStateName(string $name): self
+    public function setFilterStateName(string $name): ICities
     {
         $this->filterStateName = $name;
         return $this;
     }
 
-    public function setFilterStateAbbreviation(string $abbreviation): self
+    public function setFilterStateAbbreviation(string $abbreviation): ICities
     {
         $this->filterStateAbbreviation = $abbreviation;
         return $this;
     }
 
-    public function setTotal(int $total): self
+    public function setTotal(int $total): ICities
     {
         $this->total = $total;
         return $this;
     }
 
-    private function applyFilters(): bool
+    private function fetch(): CitiesCollection
     {
-        if ($this->filterId) {return $this->applyFilterById();}
-        if ($this->filterName) {return $this->applyFilterByName();}
-        if ($this->filterStateId) {return $this->applyFilterByStateId();}
-        if ($this->filterStateAbbreviation) {return $this->applyFilterByStateAbbreviation();}
-        if ($this->filterStateName) {return $this->applyFilterByStateName();}
-        return false;
+        if ($this->filterId) {
+            return $this->applyFilterById();
+        } else {
+            if ($this->filterStateId) {
+                $result = $this->applyFilterByStateId();
+            } else {
+                $result = $this->getAll();
+            }
+            if ($this->filterStateAbbreviation) {
+                $result = $this->applyFilterByStateAbbreviation($result);
+            }
+            if ($this->filterStateName) {
+                $result = $this->applyFilterByStateName($result);
+            }
+            if ($this->filterName) {
+                $result = $this->applyFilterByName($result);
+            }
+            return $this->arrayToCollection($result);
+        }
     }
 
-    private function getAll(): bool
+    private function getAll(): array
     {
         $data = json_decode(file_get_contents("https://servicodados.ibge.gov.br/api/v1/localidades/municipios"));
-
-        $this->arrayToCollection($data);
-        return true;
+        return $data;
     }
 
-    private function applyFilterById(): bool
+    private function applyFilterById(): CitiesCollection
     {
         $data = json_decode(file_get_contents("https://servicodados.ibge.gov.br/api/v1/localidades/municipios/{$this->filterId}"));
 
+        $State = new State(
+            $data->microrregiao->mesorregiao->UF->nome,
+            $data->microrregiao->mesorregiao->UF->id,
+            $data->microrregiao->mesorregiao->UF->sigla,
+            new Country("Brasil", 0, "BR")
+        );
         $City = new City(
             $data->nome,
             $data->id,
-            $data->microrregiao->mesorregiao->UF->nome
+            $State
         );
-        $this->Cities->add($City);
-        return true;
+        return $this->Cities->add($City);
     }
 
-    private function applyFilterByName(): bool
+    private function applyFilterByName($data): array
     {
-        $data = json_decode(file_get_contents("https://servicodados.ibge.gov.br/api/v1/localidades/municipios"));
-
         $data = array_filter($data, function($city) {
             return $city->nome === $this->filterName;
         });
 
-        $this->arrayToCollection($data);
-        return true;
+        return $data;
     }
 
-    private function applyFilterByStateId(): bool
+    private function applyFilterByStateId(): array
     {
         $data = json_decode(file_get_contents("https://servicodados.ibge.gov.br/api/v1/localidades/estados/{$this->filterStateId}/municipios"));
 
-        $this->arrayToCollection($data);
-        return true;
+        return $data;
     }
 
-    private function applyFilterByStateAbbreviation(): bool
+    private function applyFilterByStateAbbreviation($data): array
     {
-        $data = json_decode(file_get_contents("https://servicodados.ibge.gov.br/api/v1/localidades/municipios"));
-
         $data = array_filter($data, function($city) {
             return $city->microrregiao->mesorregiao->UF->sigla === $this->filterStateAbbreviation;
         });
 
-        $this->arrayToCollection($data);
-        return true;
+        return $data;
     }
 
-    private function applyFilterByStateName(): bool
+    private function applyFilterByStateName($data): array
     {
-        $data = json_decode(file_get_contents("https://servicodados.ibge.gov.br/api/v1/localidades/municipios"));
-
         $data = array_filter($data, function($city) {
             return $city->microrregiao->mesorregiao->UF->nome === $this->filterStateName;
         });
 
-        $this->arrayToCollection($data);
-        return true;
+        return $data;
     }
 
-    private function arrayToCollection($data): void
+    private function arrayToCollection($data): CitiesCollection
     {
         foreach ($data as $item) {
+            $State = new State(
+                $item->microrregiao->mesorregiao->UF->nome,
+                $item->microrregiao->mesorregiao->UF->id,
+                $item->microrregiao->mesorregiao->UF->sigla,
+                new Country("Brasil", 0, "BR")
+            );
             $City = new City(
                 $item->nome,
                 $item->id,
-                $item->microrregiao->mesorregiao->UF->nome
+                $State
             );
             $this->Cities->add($City);
         }
-        $this->Cities;
+        return $this->Cities;
     }
 }
