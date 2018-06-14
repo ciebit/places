4cfde04b8f93be1b76016c233c00c1b494899d29
Maike <maike_jbe@hotmail.com>
2018-06-14 16:19:49 -0300

    Correção de namespace e importação de classes

--- app/Repositories/IBGE/Repositories.php
+++ app/Repositories/IBGE/Repositories.php
@@ -1,7 +1,10 @@
 <?php
-namespace Places\Models\Repositories\IBGE;
+namespace Ciebit\Places\Repositories\IBGE;
 
 use Ciebit\Places\Repositories\Repositories as IRepositories;
+use Ciebit\Places\Repositories\Cities as CitiesRepository;
+use Ciebit\Places\Repositories\States as StatesRepository;
+use Ciebit\Places\Repositories\Countries as CountriesRepository;
 
 class Repositories implements IRepositories
 {
